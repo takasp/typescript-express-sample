2e5b5442712e3e3148ec8f046552bcd552390105
takasp <takasp.2350@gmail.com>
2019-10-19 15:47:34 +0900

    Fix to typescript

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
+import {Request, Response} from "express";
+
 const express = require('express');
 const index = express();
 const port = 3000;
 
-index.get('/', (req, res) => res.send('Hello World!'));
+index.get('/', (req: Request, res: Response) => res.send('Hello World!'));
 
 index.listen(port, () => console.log(`Example app listening on port ${port}!`));
