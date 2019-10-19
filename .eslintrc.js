002204ab06a2ced489bd204f66ccebb0f5f64aac
takasp <takasp.2350@gmail.com>
2019-10-19 16:39:18 +0900

    Add jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:prettier/recommended',
+    'plugin:jest/recommended',
   ],
   globals: {
     Atomics: 'readonly',
