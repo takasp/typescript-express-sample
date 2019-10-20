0531bea243ccbe2311d244e14033261041db4af3
takasp <takasp.2350@gmail.com>
2019-10-20 13:56:37 +0900

    Fix jest test

--- __test__/app.spec.ts
+++ __test__/app.spec.ts
@@ -2,8 +2,9 @@ const request = require('supertest');
 const app = require('../src/app');
 
 describe('GET /', () => {
-  it('response Hello World!', async () => {
+  it('response Hello World!', async done => {
     const response = await request(app).get('/');
     expect(response.text).toEqual('Hello World!');
+    done();
   });
 });
